fcde57776330ad6668cc969d511a0dd164d4560d
Wilmer Omar Estrada Díaz <wilmesdi@suramericana.com.co>
2019-03-31 20:25:37 -0500

    Installation and configuration of Microsoft.EntityFrameworkCore.Tools to be able to perform migrations, in addition to parametrization of database environments

--- server/viamericas/viamericas/Startup.cs
+++ server/viamericas/viamericas/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Persistence;
 
 namespace viamericas
 {
@@ -25,6 +27,8 @@ namespace viamericas
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = Configuration.GetConnectionString("Dev");
+            services.AddDbContext<AgencyDbContext>(options => options.UseSqlServer(connection));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
