5ac36ceb6faf4038dd23c40744cc8131afab6366
Wilmer Omar Estrada Díaz <wilmesdi@suramericana.com.co>
2019-03-31 20:19:52 -0500

    creation of AgencyDbContext and installation of Microsoft.EntityFrameworkCore

---
+++ server/viamericas/Persistence/AgencyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persistence
+{
+    public class AgencyDbContext : DbContext
+    {
+        public AgencyDbContext(DbContextOptions<AgencyDbContext> options)
+           : base(options)
+        { }
+
+        public DbSet<Agency> Student { get; set; }
+    }
+}
