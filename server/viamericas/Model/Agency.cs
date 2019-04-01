578812d60cee5303d6fc4828450abcc7f6c789b8
Wilmer Omar Estrada Díaz <wilmesdi@suramericana.com.co>
2019-03-31 20:14:04 -0500

    creation of agency model

---
+++ server/viamericas/Model/Agency.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class Agency
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
